0caf66eb26916dfb3eab0710d1f15803d6e59dae
Imannn1 <122066272+Imannn1@users.noreply.github.com>
2023-01-27 11:00:15 +0800

    Add files via upload

---
+++ exercise_4/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    
+    String name;
+    String car;
+
+
+    Car(String carName ,  String myName) {
+        this.car = carName;
+        this.name = myName;
+        }
+        void CarOwner() {
+            System.out.println("My name is " + this.name  + " I own This car " + this.car);
+        }
+    }
+    
+
---
+++ exercise_4/Myself.java
@@ -0,0 +1,19 @@
+public class Myself {
+    
+    String name;
+    int age;
+
+    Myself(String myName , int myAge) {
+    this.age = myAge;
+    this.name = myName;
+    }
+    void Addfriend(Person friend){
+        System.out.println("I am friends with " + friend.name);
+    
+
+}
+}
+
+
+
+
